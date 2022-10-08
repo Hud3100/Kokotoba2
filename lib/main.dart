8c22f42f643420dfbdfcd41779134a87ba065043
Hiroaki <hud3100@gmail.com>
2022-10-08 22:44:13 +0900

    言葉の保存機能の作成

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'keyboardoverlay.dart';
 import 'formatter/maxLineFormatter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -32,12 +37,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   FocusNode numberFocusNode = FocusNode();
   TextEditingController textController = TextEditingController();
   final int maxLength = 20;
   final int maxVisibleLines = 5;
   final int maxLinesForced = 5;
+  String kotoba = "";
 
   @override
   void initState() {
@@ -61,48 +66,57 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return
-    GestureDetector(
-      onTap: () {
-        FocusManager.instance.primaryFocus?.unfocus();
-      },
-      child: Scaffold(
-        body: SafeArea(
-          child: AspectRatio(
-            aspectRatio: 0.8,
-            child: Container(
-              margin: const EdgeInsets.all(0),
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-              ),
-              alignment: Alignment.centerLeft,
-              child: TextField(
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(20),
-                  MaxLinesTextInputFormatter(maxLinesForced),
-                ],
-                textAlign: TextAlign.center,
-                // textAlignVertical: TextAlignVertical.center,
-                keyboardType: TextInputType.multiline,
-                maxLength: maxLength,
-                maxLines: maxVisibleLines,
-                focusNode: numberFocusNode,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'ことばを残そう',
-                  // isDense: true,
-                  contentPadding: EdgeInsets.all(10.0)
-                ),
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                ),
-              )
-            )
-          )
-        )
-      )
-    );
+    return GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: Scaffold(
+            body: SafeArea(
+                child: AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Container(
+                        margin: const EdgeInsets.all(0),
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        child: Column(children: [
+                          TextField(
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(20),
+                              MaxLinesTextInputFormatter(maxLinesForced),
+                            ],
+                            textAlign: TextAlign.center,
+                            keyboardType: TextInputType.multiline,
+                            maxLength: maxLength,
+                            maxLines: maxVisibleLines,
+                            focusNode: numberFocusNode,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'ことばを残そう',
+                                // isDense: true,
+                                contentPadding: EdgeInsets.all(10.0)),
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 30,
+                            ),
+                            onChanged: (value) {
+                              kotoba = value;
+                            },
+                          ),
+                          TextButton(
+                            onPressed: () async {
+                              FirebaseFirestore.instance
+                                  .collection('kotobaCollection')
+                                  .doc('kotobaDocument')
+                                  .set({'text': kotoba});
+                            },
+                            child: const Text(
+                              '実行',
+                              style: TextStyle(fontSize: 50),
+                            ),
+                          ),
+                        ]))))));
   }
 }
